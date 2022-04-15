44e82b542b36e1e4c82ca80621afc5ff7d4fce80
salama <a541996salamh@gmail.com>
2022-04-15 12:18:15 +0300

    add color and fonts to style

---
+++ .history/resources/views/login.blade_20220415121602.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>login</title>
+
+</head>
+
+<body>
+
+ <div class="login-container">
+
+ </div>
+   
+<h1> تسجيل الدخول  </h1>
+</html>
--- .history/routes/web_20220329125023.php
+++ .history/routes/web_20220329125023.php
@@ -16,8 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('/login', function () {
-    return view('login');
-});
