1cd4db4fd876f0581d7f663bea16a88b5357c997
EhsasElias <ehsas.1998000@gmail.com>
2022-04-17 16:27:08 +0300

    side menu secound edit

--- resources/views/admin/layout/dashboard.blade.php
+++ resources/views/admin/layout/dashboard.blade.php
@@ -11,148 +11,133 @@
     <link rel="stylesheet" href="{{ URL::asset('css/font-awesome.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@300;400&display=swap" rel="stylesheet">
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,400;1,300&display=swap"
+        rel="stylesheet">
     <title>Cars Auction</title>
 </head>
 
 <body>
 
-    <div class="d-flex flex-nowrap">
 
-        <div class="wrapper d-flex align-items-stretch">
-            <!-- Sidebar Holder -->
-            <aside id="sidebar">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div>
-                        <div class="cornerstyle px-4 pt-3">
-                            <br>
-                            <a class="navbar-brand border p-2 text-light mx-auto  rounded" href="#">CAC</a>
-                        </div>
-                    </div>
-                    <div class="navbar-header">
-                        <button type="button" id="sidebarCollapse" class="bttn border-0 px-4">
-                            <div class="menu my-1  bg-light"> </div>
-                            <div class="menu bg-light"> </div>
-                        </button>
+    <div class="wrapper d-flex align-items-stretch">
+        <!-- Sidebar Holder -->
+        <div class="navbar-header2 my-5 d-none">
+            <button type="button" id="sidebarCollapse" class="bttn border-0 px-4">
+                <div class=" my-1 px-3 bg-light sidemenu"> </div>
+                <div class=" bg-light sidemenu"> </div>
+            </button>
+        </div>
+        <aside id="sidebar">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="logo">
+                    <div class="cornerstyle px-4 pt-3">
+                        <br>
+                        <a class="navbar-brand border p-2 text-light mx-auto logo-box" href="#">CAC</a>
                     </div>
                 </div>
-
-                <ul class="list-unstyled components fs-5 mt-4">
-
-                    <li class="active dropdown">
-                        <a class="dropdown-toggle text-center text-light" data-toggle="collapse"
-                            aria-expanded="false">إدارة
-                            المزادات</a>
-                        <ul class="collapse list-unstyled fs-6 me-5" id="manage">
-                            <li><a href="#" class="text-light p-3">إدارة طلبات تقديم المزاد</a></li>
-                            <li><a href="#" class="text-light p-3">إدارة العروض</a></li>
-                            <li><a href="#" class="text-light p-3">إدارة المزادات الحالية</a></li>
-                            <li><a href="#" class="text-light p-3">إدارة المزادات المنتهية</a></li>
-
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#" class="text-center p-3 text-light">إدارة المستخدمين</a>
-
-                    </li>
-                    <li>
-                        <a href="#" class="text-center p-3 text-light">إدارة تصنيفات السيارات</a>
-
-                    </li>
-                    <li>
-                        <a href="#" class="text-center p-3 text-light">إدارة موديل السيارة</a>
-
-                    </li>
-
-                </ul>
-
-                <ul class="list-unstyled  fs-6 py-4">
-                    <div class="d-flex flex-wrap justify-content-around w-50 m-auto">
-                        <div class="fa fa-facebook  text-light"></div>
-                        <div class="fa fa-instagram  text-light"></div>
-                        <div class="fa fa-whatsapp  text-light"></div>
-                        <div class="fa fa-facebook text-light"></div>
-                        <div class="fa  fa-envelope-o text-light"></div>
-                    </div>
-                    <li><a class="nav-item btn contact text-light fs-5 py-2 m-5 mt-3"> <i class="fa fa-phone">&nbsp;
-                                777(777)- 777</i></a>
-                    </li>
-                </ul>
-            </aside>
-
-            <!-- Page Content Holder -->
-            <div id="content">
-
-                <nav class="navbar navbar-default">
-                    <div class="container-fluid">
-
-                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                            <ul class="nav navbar-nav navbar-right">
-                                <li><a href="#">Page</a></li>
-                                <li><a href="#">Page</a></li>
-                                <li><a href="#">Page</a></li>
-                                <li><a href="#">Page</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-
-
-
+                <div class="navbar-header">
+                    <button type="button" id="sidebarCollapse" class="bttn border-0 px-4">
+                        <div class="menu my-1  bg-light"> </div>
+                        <div class="menu bg-light"> </div>
+                    </button>
+                </div>
 
             </div>
 
-        </div>
+            <ul class="list-unstyled components fs-6 mt-4">
 
+                <li class="active dropdown">
+                    <a class="dropdown-toggle text-center text-light pb-2" data-toggle="collapse"
+                        aria-expanded="false">إدارة
+                        المزادات</a>
+                    <ul class="collapse list-unstyled fs-6" id="manage">
+                        <li><a href="#" class="text-light text-center p-3">إدارة طلبات تقديم المزاد</a></li>
+                        <li><a href="#" class="text-light text-center p-3">إدارة العروض</a></li>
+                        <li><a href="#" class="text-light text-center p-3">إدارة المزادات الحالية</a></li>
+                        <li><a href="#" class="text-light text-center p-3">إدارة المزادات المنتهية</a></li>
 
-        <!-- start content -->
+                    </ul>
+                </li>
+                <li>
+                    <a href="#" class="text-center p-3 text-light">إدارة المستخدمين</a>
 
+                </li>
+                <li>
+                    <a href="#" class="text-center p-3 text-light">إدارة تصنيفات السيارات</a>
 
+                </li>
+                <li>
+                    <a href="#" class="text-center p-3 text-light">إدارة موديل السيارة</a>
 
-        <!-- head  content -->
+                </li>
 
+            </ul>
 
-        <div class="w-100" style="background-image: url(assets/back.jpg) ; background-size: contain;">
-
-            <hr class="m-0 text-white ">
-            <!-- Example single danger button -->
-            <div class="d-flex flex-lg-row-reverse m-3 gap-4">
-
-
-                <div class="dropdown-avatar">
-                    <div class="dropbtn-avatar">
-                        <img class=" rounded-circle" src="assets/images/avatar.jpg" alt="" width="32px" height="">
-                    </div>
-
-                    <div class="dropdown-content-avatar">
-                        <a href="#">اسم المستخدم</a>
-                        <hr class=" m-0 text-white">
-                        <a href="#">تسجيل الخروج</a>
-
-                    </div>
+            <ul class="list-unstyled  fs-6 py-4">
+                <div class="d-flex flex-wrap justify-content-around w-50 m-auto">
+                    <div class="fa fa-facebook  text-light"></div>
+                    <div class="fa fa-instagram  text-light"></div>
+                    <div class="fa fa-whatsapp  text-light"></div>
+                    <div class="fa fa-facebook text-light"></div>
+                    <div class="fa  fa-envelope-o text-light"></div>
                 </div>
-                <style>
-                </style>
-                <div><img src="assets/icons/bell.png" alt="" width="32px" height=""></div>
+                <li><a class="nav-item text-center contact text-light fs-5 py-2 m-5 mt-3"> <i
+                            class="fa fa-phone px-3 fs-5">&nbsp;
+                            777(777)- 777</i></a>
+                </li>
+            </ul>
+        </aside>
+        <div class="text-light me-auto mt-5">
+            <p class="fa fa-bell px-2 position-relative "><i class="notiy  position-absolute"></i></p>
+            <p class="fa fa-wechat px-2"></p>
+            <p class="fa fa-user px-2"></p>
+            <ul class="dropdown-menu notification bg-dark">
+                <li><a class="dropdown-item text-light fs-7" href="#">تمت المزايدة على سيارة هويوندا
+                        <i class="semiOrange fs-8 "><br>المشتري : احساس</i></a>
+                    <p class="dropdown-divider mx-2"></p>
+                </li>
 
-            </div>
+                <li><a class="dropdown-item text-light fs-7" href="#">تمت المزايدة على سيارة هويوندا
+                        <i class="semiOrange fs-8 "><br>المشتري : احساس</i></a>
+                    <p class="dropdown-divider mx-2"></p>
+                </li>
+                <li><a class="dropdown-item text-light fs-7" href="#">تمت المزايدة على سيارة هويوندا
+                        <i class="semiOrange fs-8 "><br>المشتري : احساس</i></a>
+                    <p class="dropdown-divider mx-2"></p>
+                </li>
+            </ul>
+            <ul class="dropdown-menu bg-dark userinfo">
+                <li>
+                    <h6 class="dropdown-item text-light" href="#">اسم المستخدم
+                    </h6>
 
-            <!-- body  content -->
-            <hr class=" text-white ">
+                </li>
 
-            @yield('content')
-         
+
+                <li><a class="dropdown-item text-light fs-7" href="#">تسجيل الخروج</a>
+                    <p class="dropdown-divider mx-2"></p>
+                </li>
+            </ul>
         </div>
+        <!-- Page Content Holder -->
+
+        <div id="content">
+            <div class="w-100" style="background-image: url(assets/back.jpg) ; background-size: contain;">
+            </div>
+        </div>
+    </div>
+
+    @yield('content')
+    {{-- <div style="background-repeat: no-repeat;
+     background-image: url(assets/back.jpg); background-size:cover"></div>
+
+
+    <img class="back-dash" src="assets/back.jpg" alt=""> --}}
+
 </body>
-
-
 
 
 <footer>
--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,9 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 
-@extends('layouts.head')
-@section('header')
-@endsection
+    <head>
+        <meta charset="UTF-8" />
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="{{ URL::asset('css/dashboard.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('css/font-awesome.css') }}">
+    
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@300;400&display=swap" rel="stylesheet">
+        <title>Cars Auction</title>
+    
+    </head>
     <!-- Font Family -->
 
 <body class="fs-6">
@@ -69,9 +81,14 @@
 
 
     <!-- nav end -->
-@extends('layouts.footer')
-@section('footer')
-@endsection
+    
+    <footer>
+        <script src="{{ URL::asset('js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+        <script src="{{ URL::asset('js/js.js') }}"></script>
+        <script src="{{ URL::asset('js/app.js') }}"></script>
+        
+    </footer>
 </body>
 
 </html>
