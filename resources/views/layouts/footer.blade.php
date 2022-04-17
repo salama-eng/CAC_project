2f8e3bf64346a7174c61d539b3c2308598990834
salama <a541996salamh@gmail.com>
2022-04-17 09:57:29 +0300

    relations

--- resources/views/layouts/footer.blade.php
+++
@@ -1,8 +0,0 @@
-@yield('footer')
-<footer>
-    <script src="{{ URL::asset('js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
-    <script src="{{ URL::asset('js/js.js') }}"></script>
-    <script src="{{ URL::asset('js/app.js') }}"></script>
-    
-</footer>
