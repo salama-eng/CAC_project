7b385bd4409b16c0a6e0a32ff8e8be81e7d9eb37
Khalifa-Alqiadi <khalifa.alqiadi@gmail.com>
2022-04-17 23:01:41 +0300

    commit init
    add curb in adminModels file

--- resources/views/admin/adminModels.blade.php
+++ resources/views/admin/adminModels.blade.php
@@ -1,10 +1,15 @@
 @extends('admin.layout.dashboard')
 @section('content')
 
-
+@if($do == 'Manage')
 
 <h1 class="text-center  text-white">ادارة الموديلات</h1>
     <div class="container">
+        @if(session()->has('success'))
+            <div class="alert alert-success">
+                {{ session()->get('success') }}
+            </div>
+        @endif
         <a href="" class="btn1 btn-sm text-white" data-bs-toggle="modal" data-bs-target="#addModel">
             <i class="fa fa-plus"></i> اضافة موديل
         </a>
@@ -21,7 +26,7 @@
                     <td>1</td>
                     <td>{{$model->name}}</td>
                     <td>
-                        <a href="" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#editModel{{$model->id}}">
+                        <a href="adminModels?do=Edit&modelid={{$model->id}}" class="btn btn-success">
                             <i class='fa fa-edit'></i> Edit
                         </a>
                         <a href="" class='btn btn-danger' data-bs-toggle="modal" data-bs-target="#deleteModel{{$model->id}}">
@@ -39,35 +44,6 @@
                         
                     </td>
                 </tr>
-                <div class="modal fade user" id="editModel{{$model->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <form action="edit_admin_model" method="post" >
-                                @csrf
-                                <div class="modal-header">
-                                    <h5 class="modal-title  text-white" id="exampleModalLabel">تعديل الموديل</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <input type="hidden" name="companyid" value="{{$model->id}}">
-                                    <!-- Start Model -->
-                                    <div class="mb-2 row">
-                                        <label class="col-sm-3 col-form-label">اسم الموديل</label>
-                                        <div class="col-sm-8 col-md-9">
-                                            <input type="text" name="model" value="{{$model->name}}" class="form-control" autocomplete="off" required="required" placeholder="ادخل اسم الموديل">
-                                        </div>
-                                    </div>
-                                    <!-- End Model -->
-                                    
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">تراجع</button>
-                                    <input type="submit" class="btn btn-primary" value="تعديل" />
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
                 <div class="modal fade user" id="deleteModel{{$model->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog">
                         <div class="modal-content">
@@ -91,7 +67,7 @@
                 </div>
                 <div class="modal fade user" id="activeModel{{$model->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog">
-                        <div class="modal-content">
+                        <div class="modal-content bg-dark">
                             <form action="active_admin_model" method="post">
                                 @csrf
                                 <div class="modal-header">
@@ -99,7 +75,7 @@
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
-                                    <input type="hidden" name="companyid" value="{{$model->id}}">
+                                    <input type="hidden" name="modelid" value="{{$model->id}}">
                                     <h2>هل انت متاكد</h2>
                                 </div>
                                 <div class="modal-footer">
@@ -114,38 +90,80 @@
             </table>
         </div>
        
-        <a href="adminModels#addModel" class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#addModel">
+        <a href="adminModels?do=Add" class="btn btn-sm btn-primary">
             <i class="fa fa-plus"></i> اضافة موديل
         </a>
     </div>
-    <div class="modal fade user" id="addModel" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content border-1 border-white">
-                <form action="add_admin_model" method="POST">
-                    @error('model') <h1>{{$message}}</h1>@enderror
-                        <h5 class="modal-title text-white" id="exampleModalLabel">اضافة موديل</h5>
-                        <button type="button" class="btn-close bg-yellow" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body border-0">
-                        <!-- Start Model -->
-                        <div class="mb-3 row">
-                            <label class="col-sm-2 col-form-label text-white">موديل جديد</label>
-                            <div class="col-sm-8 col-md-9">
-                                <input type="text" name="model" class="form-control" autocomplete="off" placeholder="اضف موديل جديد">
-                            </div>
-                        </div>
-                        <!-- End Model -->
-                        
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">تراجع</button>
-                        <input type="submit" class="btn btn-secondary text-white" value="اضافة" />
-                    </div>
-                </form>
+@elseif($do == 'Add')
+
+<h1 class="text-center">Add New Modal</h1>
+<div class="container">
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form action="add_admin_model" method="POST">
+    @csrf
+        <!-- Start Model -->
+        <div class="mb-3 row">
+            <label class="col-sm-2 col-form-label text-white">موديل جديد</label>
+            <div class="col-sm-8 col-md-9">
+                <input type="text" name="model" class="form-control" autocomplete="off" placeholder="اضف موديل جديد">
             </div>
         </div>
-    </div>
-
-           
+        <!-- End Model -->
+        <!-- Start Submit -->
+        <div class="mb-2 row">
+            <div class="offset-sm-2 col-sm-10">
+                <input type="submit" value="اضافة موديل" class=" btn btn-primary ">
+            </div>
+        </div>
+        <!-- End Submit -->
+    </form>
+</div>
+@elseif($do == 'Edit')
+{{$modelid = isset($_GET['modelid']) && is_numeric($_GET['modelid']) ? intval($_GET['modelid']) : 0;}}
+<h1 class="text-center">Edit Modal</h1>
+<div class="container">
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    @foreach($models as $model)
+        @if($model->id == $modelid)
+            <form action="edit_admin_model" method="POST">
+                @csrf
+                <input type="hidden" name="modelid" value="{{$modelid}}">
+                <!-- Start Model -->
+                <div class="mb-3 row">
+                    <label class="col-sm-2 col-form-label text-white">موديل جديد</label>
+                    <div class="col-sm-8 col-md-9">
+                        <input type="text" name="model" value="{{$model->name}}" class="form-control" autocomplete="off" placeholder="ادخل اسم الموديل">
+                    </div>
+                </div>
+                <!-- End Model -->
+                <!-- Start Submit -->
+                <div class="mb-2 row">
+                    <div class="offset-sm-2 col-sm-10">
+                        <input type="submit" value="تعديل الموديل" class=" btn btn-primary ">
+                    </div>
+                </div>
+                <!-- End Submit -->
+                
+            </form>
+        @endif
+    @endforeach
+</div>
+@endif
 @endsection
                 
