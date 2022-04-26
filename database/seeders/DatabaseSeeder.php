a8d75bd22841ade22681fecaa05c412997327d19
Donia Shaker <donia.shaker11@gmail.com>
2022-04-26 22:00:26 +0300

    Send Email

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use DB;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,8 +12,24 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+    use DB;
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('roles')->insert([
+             'name' => 'super_admin',
+            'display_name' => 'ادارة النظام', // optional
+            'description' => 'User is the owner of a given project', // optional
+        ]);
+         DB::table('roles')->insert([
+              'name' => 'admin',
+            'display_name' => 'ادارة المحتوى', // optional
+            'description' => 'User is the owner of a given project', // optional
+        ]);
+         DB::table('roles')->insert([
+              'name' => 'client',
+            'display_name' => 'العملاء',// optional
+            'description' => 'User is the owner of a given project', // optional
+        ]);
     }
 }
