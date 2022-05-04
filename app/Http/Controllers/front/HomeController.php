c4fa1207b65c1d2caba8069c0ffe51d0809b1a9e
Khalifa-Alqiadi <khalifa.alqiadi@gmail.com>
2022-05-04 22:47:17 +0300

    add roles

--- app/Http/Controllers/front/HomeController.php
+++ app/Http/Controllers/front/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\front;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use App\Models\Role;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 class HomeController extends Controller
@@ -19,5 +20,17 @@ class HomeController extends Controller
     public function errorsRedirect(){
         $route = Route::current()->getName();
         return view('front.errors', ['route' => $route]);
+    }
+    public function adminRole(){
+        $role = new Role;
+        $role->name = 'admin';
+        $role->display_name = 'management project';
+        $role->save();
+    }
+    public function clientRole(){
+        $role = new Role;
+        $role->name = 'client';
+        $role->display_name = 'website';
+        $role->save();
     }
 }
