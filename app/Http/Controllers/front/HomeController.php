9ba2f107ffefc05c2e7ddca3fcf990afa6027f11
Khalifa-Alqiadi <khalifa.alqiadi@gmail.com>
2022-05-07 18:59:31 +0300

    add command coding

--- app/Http/Controllers/front/HomeController.php
+++ app/Http/Controllers/front/HomeController.php
@@ -6,11 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Auction;
 use App\Models\membership;
 use App\Models\Post;
-<<<<<<< HEAD
-use App\Models\Role;
-=======
 use App\Models\slider_image;
->>>>>>> b06fa2679f21d5c7713e455c0537c9154b800f88
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 class HomeController extends Controller
@@ -27,21 +23,6 @@ class HomeController extends Controller
         $route = Route::current()->getName();
         return view('front.errors', ['route' => $route]);
     }
-<<<<<<< HEAD
-    public function adminRole(){
-        $role = new Role;
-        $role->name = 'admin';
-        $role->display_name = 'management project';
-        $role->save();
-        return view('front.index');
-    }
-    public function clientRole(){
-        $role = new Role;
-        $role->name = 'client';
-        $role->display_name = 'website';
-        $role->save();
-        return view('front.index');
-=======
 
     public function showHomePage(){
         $postsAll=Post::with(['users'])->get();
@@ -73,6 +54,5 @@ class HomeController extends Controller
             'Posts' => $posts,
         ]);
 
->>>>>>> b06fa2679f21d5c7713e455c0537c9154b800f88
     }
 }
