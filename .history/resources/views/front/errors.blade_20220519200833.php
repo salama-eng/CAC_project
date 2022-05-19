787b0b95f378445c8499ba0c2925eef25a6edba1
salama <a541996salamh@gmail.com>
2022-05-19 20:29:08 +0300

    commit

--- .history/resources/views/front/errors.blade_20220519200833.php
+++ .history/resources/views/front/errors.blade_20220519200833.php
@@ -14,7 +14,7 @@
         <section class="container d-flex mt-4 vh-100 align-items-center text-center justify-content-center">
             <div class="d-flex flex-wrap mt-4 justify-content-center align-items-center text-center">
                 <h6 class="col-10 mt-4">  اوبس! حسابك غير مفعل </h6>
-                <img class=" image-fluid m-auto col-9 col-lg-4" src="assets/images/rigester.png"  height=""alt="">
+                <img class=" image-fluid m-auto col-9 col-lg-4" src="assets/images/error.png"  height=""alt="">
                 <a href="https://mail.google.com/mail/" class="card-link active text-center mt-3 mb-2 col-12">   تفعيل حسابك  
                     <i class="fa fa-long-arrow-left p-2 pt-1"> </i>
                 </a>
