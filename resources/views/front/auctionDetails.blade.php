1a358b54a2414d0572fe272a18324e51f2b7f543
Khalifa-Alqiadi <khalifa.alqiadi@gmail.com>
2022-05-21 00:06:24 +0300

    add roles

--- resources/views/front/auctionDetails.blade.php
+++ resources/views/front/auctionDetails.blade.php
@@ -170,7 +170,7 @@
                             aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog">
                                 <div class="modal-content  ">
-                                    <form action="{{ route('test') }}" method="get">
+                                    <form action="{{ route('test') }}" method="post">
                                         @csrf
                                         <div class="modal-header bg-darkgrey">
                                             <input type="hidden" name="post_id" value="{{$post->id}}">
