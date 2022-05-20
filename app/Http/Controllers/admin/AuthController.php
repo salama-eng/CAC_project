f4ee417fb52239013a86dab8776b5cdfe0edb3ca
Khalifa-Alqiadi <khalifa.alqiadi@gmail.com>
2022-05-20 19:54:01 +0300

    add roles

--- app/Http/Controllers/admin/AuthController.php
+++ app/Http/Controllers/admin/AuthController.php
@@ -98,10 +98,11 @@ class AuthController extends Controller
         $u->name = $request->name;
         $u->password = Hash::make($request->password);
         $u->email = $request->email;
+        $u->is_active = 1;
         $token = Str::uuid();
         $u->remember_token = $token;
         if ($u->save()) {
-            $u->attachRole('client');
+            $u->attachRole('admin');
             $email_data = array(
                 'name' => $request->name, 'email' => $request->email, 'password' => $v,
                 'activation_url' => URL::to('/') . "/verify_email/" . $token . "/".$v
