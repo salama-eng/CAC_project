3926da6d8fda3515a12214a39778bc3a2a6f5f91
Khalifa-Alqiadi <khalifa.alqiadi@gmail.com>
2022-05-22 10:33:07 +0300

    edit payment

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -133,7 +133,7 @@ class testController extends Controller
                     ]];
                   $curl = curl_init();
                   curl_setopt_array($curl, array(
-                      CURLOPT_URL                 => "https://waslpayment.com/api/test/merchant/payment_order",
+                      CURLOPT_URL                 => "https://waslpayment.com/api/v1/merchant/payment_order",
                       CURLOPT_RETURNTRANSFER      => true,
                       CURLOPT_ENCODING            => "",
                       CURLOPT_MAXREDIRS           => 10,
